fce44bc222d10d64c0c5b3bc02671b5074103c56
Jahintaqi Chisty <jchisty.bel@baygroupco.com>
2019-10-22 16:15:07 +0600

    Update

--- trydjango/vol/views.py
+++ trydjango/vol/views.py
@@ -5,3 +5,7 @@ from django.http import request
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+#For news link 
+def news(request):
+    return render(request,'news.html')

f78d97bd12eac44330f99bfb4c753d863d8e8b1c
Jahintaqi Chisty <jchisty.bel@baygroupco.com>
2019-11-06 11:51:06 +0600

    Change in VOL

--- trydjango/vol/urls.py
+++ trydjango/vol/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns= [
     path('',views.index,name="index"),
-    path('news/',views.news, name="news")
+    path('news/',views.news, name="news"),
+    path('contact/',views.contact, name="contact"),
 ]
--- trydjango/vol/views.py
+++ trydjango/vol/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import request
 
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
-#For news link 
+    # For news link
+
+
 def news(request):
-    return render(request,'news.html')
+    return render(request, 'news.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
